d0b91421855e4f4be3325e76d4255ef8fffaca28
Matt South <matt.south@openclinical.net>
2025-04-17 16:06:07 +0100

    tweak to demo, based on working version (3.1.1)

--- imports/ui/Hello.vue
+++ imports/ui/Hello.vue
@@ -15,8 +15,8 @@ autorun(() => currentUser.value = Meteor.user())
 
 <template>
   <template v-if="currentUser">
-    <h2 class="text-2xl font-bold my-3" :class="'text-'+currentUser.colour">Hi {{currentUser.username}}!</h2>
-  </template>`
+    <h2 class="text-2xl font-bold my-3" :style="'color: ' + currentUser.colour">Hi {{currentUser.username}}!</h2>
+  </template>
   <button @click="increment" class="bg-green-700 hover:bg-green-900 text-white py-2 px-4 rounded">Click Me</button>
   <p>
     You've pressed the button <strong>{{ count }}</strong> times.
